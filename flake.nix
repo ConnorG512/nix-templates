e3bf45733e8533f51236985c085623bc7dcd7ef3
Connor Garey <connorgarey1999@gmail.com>
2025-11-18 20:59:27 +0000

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   description = "Templates list Flake";
   
-  outputs = { self, ... }: 
+  outputs = { self, ... }: {
     templates = {
       basic-cpp23 = {
         path = ./basic-cpp23;
         description = "A very basic C++23 template with release and debug builds.";
       };
     };
-    
     defaultTemplate = self.templates.basic-cpp23;
+  };
 }
