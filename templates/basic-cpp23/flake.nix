24f823ddd8063a925aca109efceec3dbd31622d1
Connor Garey <connorgarey1999@gmail.com>
2025-11-25 18:12:08 +0000

    Moved derivation to let in and added projectInfo

--- templates/basic-cpp23/flake.nix
+++ templates/basic-cpp23/flake.nix
@@ -10,17 +10,60 @@
     system = "x86_64-linux";
     pkgs =  import nixpkgs { inherit system; };   
 
+    projectInfo = {
+      projectName = "App";
+      execName = "app";
+      version = "indev";
+    };
+
+    derivations = {
+      release = pkgs.stdenv.mkDerivation {
+        pname = projectInfo.projectName;
+        name = projectInfo.execName;
+        version = projectInfo.version; 
+        src = ./.;
+          
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+        ];
+        
+        buildInputs = with pkgs; [ ];
+        
+        cmakeFlags = [
+          "-DCMAKE_BUILD_TYPE=Release"
+          "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+        ];
+        
+        configurePhase = ''
+          cmake -B build -S . -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
+        '';
+
+        buildPhase = ''
+          cmake --build build 
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp build/compile_commands.json $out/bin
+          cp build/${projectInfo.execName} $out/bin
+        '';
+      };
+    };
   in {
     devShells.x86_64-linux.default = pkgs.mkShellNoCC {
       packages = with pkgs; [
         ccls
-
-        cmake
-        ninja
+        
+        gef
+        strace 
       ];
       
       shellHook = ''
+        echo "Entering shell!"
       '';
     }; 
+    
+    packages.x86_64-linux.default = derivations.release;
   };
 }
